95771525166d197d21212ef01d843687261ebc4a
egarcode95 <egarcode95@gmail.com>
2023-09-01 16:37:08 -0600

    código de otros

---
+++ filtro.js
@@ -0,0 +1,67 @@
+// Tenemos un li de productos
+
+const productos = [
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+]
+
+ let li = document.getElementById("lista-de-productos");//cambiar getElementByName a ById
+const $i = document.querySelector('.input');
+
+ function displayProductos(productosArray) { //crear funcion para ejecutar el loop
+
+for (let i = 0; i < productosArray.length; i++) {
+  var d = document.createElement("div");
+  d.classList.add("producto");
+
+  var ti = document.createElement("p");
+  ti.classList.add("titulo");
+  ti.textContent = productosArray[i].nombre;
+  
+  var imagen = document.createElement("img");
+  imagen.setAttribute('src', productosArray[i].img);
+
+  d.appendChild(ti);
+  d.appendChild(imagen);
+
+  li.appendChild(d);
+}
+ }
+
+displayProductos(productos);//Esta funcion estaba siendo invocada pero no se habia decladrado
+
+const botonDeFiltro = document.querySelector("button");
+
+botonDeFiltro.onclick = function() {
+  while (li.firstChild) {
+    li.removeChild(li.firstChild);
+  }
+
+  const texto = $i.value;
+  console.log(texto);
+  const productosFiltrados = filtrado(productos, texto );
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div");
+    d.classList.add("producto");
+  
+    var ti = document.createElement("p");
+    ti.classList.add("titulo");
+    ti.textContent = productosFiltrados[i].nombre;
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productosFiltrados[i].img);
+  
+    d.appendChild(ti);
+    d.appendChild(imagen);
+  
+    li.appendChild(d);
+  }
+}
+
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+}  
